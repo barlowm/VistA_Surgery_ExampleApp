625468d7ff434d5cd21f2c36bb5ac8b3f768129e
barlowm <barlowm@gmail.com>
2016-11-17 11:24:47 -0500

    Uploading the REST of the files

---
+++ build/temp/testing/VistA.Surgery/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'D:\WebRoot\htdocs\sencha\App2\VistA_Surgery/build/.sass-cache'
---
+++ build/temp/testing/VistA.Surgery/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require D:\WebRoot\htdocs\sencha\App2\VistA_Surgery\app.js
